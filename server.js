dea83fb226d3245105ce1d10e578d95e8e0aef62
Computer_Account <computer_account@192-168-1-114.tpgi.com.au>
2018-10-02 02:51:08 +1000

    fixed: path to dist

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'production';
 const app = express();
+const publicPath = path.resolve(__dirname, 'dist/');
 
-app.use(express.static(__dirname));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname+'/dist/index.html');
+    res.sendFile(path.resolve(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
